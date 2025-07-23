2834bbd8ce919434414c2b7840b6e32c3f2618ac
kingdart98 <kmay98@outlook.com>
2025-07-23 10:35:16 -0400

    Adding in the player code

--- main.py
+++ main.py
@@ -1,23 +1,40 @@
-# mandatory pygame initialization
 import pygame
-
-pygame.init()
-
-
-
-# other imports as needed
 from constants import *
+from player import Player
+from AsteroidField import *
 
-# the good stuff
+
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    color = (0, 0, 0)  # black background
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
     while True:
-        pygame.Surface.fill(screen, color)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        updatable.update(dt)
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
